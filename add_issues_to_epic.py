9960a12a9da3b2199d380d8700774680f279b7bf
john8739 <jsolly@esri.com>
2020-05-11 11:51:19 -0700

    Refactoring

--- add_issues_to_epic.py
+++ add_issues_to_epic.py
@@ -1,11 +1,11 @@
 import json
 import requests
 from other.my_secrets import DEVTOPIA_TOKEN, ZENTOPIA_API_URL, ZENTOPIA_REPO_ID
-from GitHub.GithubScripts import get_stuff
+from GitHub.HelperScripts import get_funcs
 
 
 def add_issues_to_epic_from_issue_body(issue_number, epic_issue_number):
-    issue_numbers = get_stuff.get_issue_numbers_from_issue_body(issue_number)
+    issue_numbers = get_funcs.get_issue_numbers_from_issue_body(issue_number)
     epic_issue_id = epic_issue_number
 
     json_data = {
