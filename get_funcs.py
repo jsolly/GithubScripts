e768e27e7789980bc104e60efef0c680fd738104
john8739 <jsolly@esri.com>
2020-06-05 09:58:15 -0700

    refactoring

--- get_funcs.py
+++ get_funcs.py
@@ -1,12 +1,17 @@
 import requests
 import json
 import re
-from other.my_secrets import get_devtopia_api_obj, ZENTOPIA_DICT
+from other.my_secrets import MySecrets
+import urllib3
 
-ZENTOPIA_API_URL = ZENTOPIA_DICT["ZENTOPIA_DICT"]
-ZENTOPIA_REPO_ID = ZENTOPIA_DICT["ZENTOPIA_REPO_ID"]
-ZENTOPIA_TOKEN = ZENTOPIA_DICT["ZENTOPIA_TOKEN"]
-DEVTOPIA_REPO_OBJECT = get_devtopia_api_obj()
+urllib3.disable_warnings()
+
+ZENTOPIA_DICT = MySecrets.ZENTOPIA_DICT
+ZENTOPIA_API_URL = ZENTOPIA_DICT["API_URL"]
+ZENTOPIA_REPO_ID = ZENTOPIA_DICT["REPO_ID"]
+ZENTOPIA_TOKEN = ZENTOPIA_DICT["TOKEN"]
+ZENTOPIA_RELEASE_ID = ZENTOPIA_DICT["8.2_Release_ID"]
+DEVTOPIA_REPO_OBJECT = MySecrets.get_devtopia_api_obj()
 
 
 pattern = re.compile(r"https://.*/issues/\d*")
@@ -33,19 +38,25 @@ def get_epic_issue_numbers(epic_issue_number: int):
     return epic_issue_numbers
 
 
-def get_issues_to_verify_after_release():
-    time_in_datetime = datetime.strptime("07-02-2019", "%d-%m-%Y")
+def get_issues_to_verify_after_release(release_id, repo_id):
+    release_issues_url = f"{ZENTOPIA_API_URL}/p1/reports/release/{release_id}/issues"
+    head = {"X-Authentication-Token": ZENTOPIA_TOKEN}
+    release_issues = requests.request(
+        method="GET", url=release_issues_url, headers=head, verify=False
+    ).json()
 
-    issues = list(REPO.get_issues(state="closed", since=time_in_datetime))
-    print(len(issues))
-    for issue in issues:
-        if "pull" in issue.html_url:
-            continue
-        print(issue.title)
-        print(f"- [ ] {issue.html_url}")
-        print(f"created by {issue.user.name}. Closed on {issue.closed_at}")
-        print("\n")
+    issue_numbers = [issue_dict["issue_number"] for issue_dict in release_issues]
+
+    for issue_number in issue_numbers:
+        issue_data_url = (
+            f"{ZENTOPIA_API_URL}/p1/repositories/{repo_id}/issues/{issue_number}"
+        )
+        issue_data = requests.request(
+            method="GET", url=issue_data_url, headers=head, verify=False
+        ).json()
+        if "pipeline" in issue_data:
+            print(issue_number)
 
 
 if __name__ == "__main__":
-    get_epic_issue_numbers(3591)
+    get_issues_to_verify_after_release(ZENTOPIA_RELEASE_ID, ZENTOPIA_REPO_ID)
